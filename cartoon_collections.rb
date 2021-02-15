bb7a5c56f150133d55a337a51420e35dc79d78ab
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-02-14 22:08:56 -0600

    commit tree 59e7859a37d2a348b12472a82c414b76a01e56c0

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{ |x, i| puts "#{i + 1}. #{x}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  return array.map{ |x| x.capitalize << "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  return array.any?{ |x| x.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if (array.any?{ |x| cheese_types.include?(x) })
+      found = ""
+
+      array.each_with_index do |item, i|
+        if (cheese_types.index(item) != nil)
+          found = item
+        end 
+      end
+
+      return found
+  else
+    return nil
+  end
 end
